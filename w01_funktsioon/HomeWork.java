b4059d5031de6a784405801503fde561af450787
filiptaik <filip1@tlu.ee>
2020-02-11 09:21:14 +0200

    ääaäaäaäaääa

--- w01_funktsioon/HomeWork.java
+++ w01_funktsioon/HomeWork.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class HomeWork {
     public static void main(String[] args) {
-        int num = 5;
+        Scanner myNum = new Scanner(System.in); 
+        System.out.println("Sisesta number");
+        int num =myNum.nextInt();
         for(int i = 1; i <= 10; ++i)
         {
             System.out.printf("%d * %d = %d \n", num, i, num * i);
