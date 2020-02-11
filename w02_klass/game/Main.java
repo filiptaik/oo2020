906a3a360a1490f4ce0cd8d90600fb10692cef29
filiptaik <filip1@tlu.ee>
2020-02-11 11:33:32 +0200

    teine tund, mängu classid

---
+++ w02_klass/game/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args){
+        Character filip = new Character("Filip", CharacterType.GOBLIN);
+       filip.slogan = "äaääa"; 
+       System.out.println(filip);
+    }
+    
+}
